3409d056053acc0e16464e7652259129676b63a7
M.Dybizbanski <dybizbanski@gmail.com>
2015-12-23 12:28:48 +0100

    Podio margin fix

--- scripts/third_party/podio.js
+++ scripts/third_party/podio.js
@@ -314,6 +314,10 @@ Sidebar.cssUpdate = [
         selector: "#global-container",
         property: "margin-left",
         value   : "50px"
+    },{
+        selector: ".global-container",
+        property: "margin-left",
+        value   : "50px"
     },
     {
         selector: "#header-global",
